b28bb0f96bb299ddb133826b2d0462ea0813b704
Albert I <kras@raphielgang.org>
2020-12-03 00:46:26 +0800

    prima: Fix non-diag build
    * Guard WLAN_LOGGING_SOCK_SVC_ENABLE under BUILD_DIAG_VERSION=1
    * This is potentially disastrous for getting logs from WLAN driver which
      highly depends on diag features, so handle with care.
    
    Test: Compile with BUILD_DIAG_VERSION={0|1}, build completes
    [Forward-port to LA.UM.9.6.2.r1]
    (cherry picked from commit 8b38c7581a4726c88f825d49af8d6e96feaf9ddd)
    Change-Id: 5128e04bd4cbef1d5f85b5e0e0ca4e658f029ccc
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -10991,9 +10991,9 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    send_btc_nlink_msg(WLAN_MODULE_DOWN_IND, 0);
 
    hdd_close_tx_queues(pHddCtx);
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    wlan_free_fwr_mem_dump_buffer();
 
-#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    if (pHddCtx->cfg_ini->wlanLoggingEnable)
    {
        wlan_logging_sock_deactivate_svc();
@@ -11681,6 +11681,7 @@ void hdd_init_frame_logging_done(void *fwlogInitCbContext, tAniLoggingInitRsp *p
       return;
    }
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    /*Check feature supported by FW*/
    if(TRUE == sme_IsFeatureSupportedByFW(MEMORY_DUMP_SUPPORTED))
    {
@@ -11691,6 +11692,7 @@ void hdd_init_frame_logging_done(void *fwlogInitCbContext, tAniLoggingInitRsp *p
    {
       wlan_store_fwr_mem_dump_size(0);
    }
+#endif
 
 
 }
@@ -12688,11 +12690,9 @@ int hdd_wlan_startup(struct device *dev )
        hddLog(VOS_TRACE_LEVEL_INFO, FL("Logging disabled in ini"));
    }
 
-#endif
-
-
    if (vos_is_multicast_logging())
        wlan_logging_set_log_level();
+#endif
 
    hdd_register_mcast_bcast_filter(pHddCtx);
    if (VOS_STA_SAP_MODE != hdd_get_conparam())
@@ -12818,8 +12818,8 @@ int hdd_wlan_startup(struct device *dev )
 
    goto success;
 
-err_open_cesium_nl_sock:
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
+err_open_cesium_nl_sock:
    hdd_close_cesium_nl_sock();
 #endif
 
@@ -14141,6 +14141,7 @@ VOS_STATUS hdd_issta_p2p_clientconnected(hdd_context_t *pHddCtx)
 }
 
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 /*
  * API to find if the firmware will send logs using DXE channel
  */
@@ -14166,6 +14167,8 @@ v_U8_t hdd_is_fw_ev_logging_enabled(void)
 
     return (pHddCtx && pHddCtx->cfg_ini->enableFWLogging);
 }
+#endif
+
 /*
  * API to find if there is any session connected
  */
--- CORE/VOSS/src/vos_api.c
+++ CORE/VOSS/src/vos_api.c
@@ -1703,6 +1703,7 @@ VOS_STATUS vos_free_context( v_VOID_t *pVosContext, VOS_MODULE_ID moduleID,
 } /* vos_free_context() */
 
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 bool vos_is_log_report_in_progress(void)
 {
     return wlan_is_log_report_in_progress();
@@ -1713,9 +1714,6 @@ void vos_reset_log_report_in_progress(void)
     return wlan_reset_log_report_in_progress();
 }
 
-
-
-
 int vos_set_log_completion(uint32 is_fatal,
                             uint32 indicator,
                             uint32 reason_code)
@@ -1723,13 +1721,16 @@ int vos_set_log_completion(uint32 is_fatal,
     return wlan_set_log_completion(is_fatal,
                                    indicator,reason_code);
 }
+#endif
 
 void vos_get_log_and_reset_completion(uint32 *is_fatal,
                              uint32 *indicator,
                              uint32 *reason_code,
                              bool reset)
 {
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     wlan_get_log_and_reset_completion(is_fatal, indicator, reason_code, reset);
+#endif
 }
 
 
@@ -1822,6 +1823,7 @@ VOS_STATUS __vos_fatal_event_logs_req( uint32_t is_fatal,
                         bool wait_required,
                         bool dump_vos_trace)
 {
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     VOS_STATUS vosStatus;
     eHalStatus status;
     VosContextType *vos_context;
@@ -1843,9 +1845,11 @@ VOS_STATUS __vos_fatal_event_logs_req( uint32_t is_fatal,
 
     if(!pHddCtx->cfg_ini->wlanLoggingEnable)
     {
+#endif
        VOS_TRACE(VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_INFO,
             "%s: Wlan logging not enabled", __func__);
         return VOS_STATUS_E_FAILURE;
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     }
 
     if (!pHddCtx->cfg_ini->enableFatalEvent || !pHddCtx->is_fatal_event_log_sup)
@@ -1919,6 +1923,7 @@ VOS_STATUS __vos_fatal_event_logs_req( uint32_t is_fatal,
         return VOS_STATUS_SUCCESS;
     else
         return VOS_STATUS_E_FAILURE;
+#endif
 }
 
 VOS_STATUS vos_fatal_event_logs_req( uint32_t is_fatal,
@@ -1954,7 +1959,9 @@ VOS_STATUS vos_fatal_event_logs_req( uint32_t is_fatal,
 
 VOS_STATUS vos_process_done_indication(v_U8_t type, v_U32_t reason_code)
 {
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     wlan_process_done_indication(type, reason_code);
+#endif
     return VOS_STATUS_SUCCESS;
 }
 
@@ -1964,8 +1971,10 @@ VOS_STATUS vos_process_done_indication(v_U8_t type, v_U32_t reason_code)
  */
 void vos_flush_host_logs_for_fatal(void)
 {
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    wlan_flush_host_logs_for_fatal();
    return;
+#endif
 }
 
 
@@ -3366,8 +3375,10 @@ void vos_probe_threads(void)
         return;
     } else if (ring_id == RING_ID_PER_PACKET_STATS) {
         vos_context->packet_stats_log_level = log_val;
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
         if (WLAN_LOG_LEVEL_ACTIVE != log_val)
             wlan_disable_and_flush_pkt_stats();
+#endif
 
         return;
     }
@@ -3561,15 +3572,18 @@ void get_rate_and_MCS(per_packet_stats *stats, uint32 rateindex)
     stats->MCS.short_gi = ratetbl->short_gi;
 }
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 bool vos_isPktStatsEnabled(void)
 {
     bool value;
     value = wlan_isPktStatsEnabled();
     return (value);
 }
+#endif
 
 bool vos_is_wlan_logging_enabled(void)
 {
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     v_CONTEXT_t vos_ctx = vos_get_global_context(VOS_MODULE_ID_SYS, NULL);
     hdd_context_t *hdd_ctx;
 
@@ -3589,11 +3603,14 @@ bool vos_is_wlan_logging_enabled(void)
 
     if (!hdd_ctx->cfg_ini->wlanLoggingEnable)
     {
+#endif
        hddLog(VOS_TRACE_LEVEL_FATAL,"%s: Logging framework not enabled!", __func__);
        return false;
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
     }
 
     return true;
+#endif
 }
 
 /**---------------------------------------------------------------------------
