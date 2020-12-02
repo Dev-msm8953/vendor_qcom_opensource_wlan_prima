b9c51e0672ae876cb63c4242a887bf5b90549a8f
Albert I <kras@raphielgang.org>
2020-12-02 14:14:44 +0800

    prima: Fix Clang warnings by comparing against NULL values
    (cherry picked from commit 88c206efa0acd009a7ae247f05473f6b22c9c510)
    Change-Id: 61589723a03b839eb262454853e16a00da867db0
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessMlmReqMessages.c
@@ -3962,7 +3962,7 @@ limProcessMinChannelTimeout(tpAniSirGlobal pMac)
         {
             // This shouldn't be the case, but when this happens, this timeout should be for the last channelId. 
             // Get the channelNum as close to correct as possible.
-            if(pMac->lim.gpLimMlmScanReq->channelList.channelNumber)
+            if(pMac->lim.gpLimMlmScanReq->channelList.channelNumber != NULL)
             {
                 channelNum = pMac->lim.gpLimMlmScanReq->channelList.channelNumber[pMac->lim.gpLimMlmScanReq->channelList.numChannels - 1];
             }
@@ -4038,7 +4038,7 @@ limProcessMaxChannelTimeout(tpAniSirGlobal pMac)
         }
         else
         {
-            if(pMac->lim.gpLimMlmScanReq->channelList.channelNumber)
+            if(pMac->lim.gpLimMlmScanReq->channelList.channelNumber != NULL)
             {
                 channelNum = pMac->lim.gpLimMlmScanReq->channelList.channelNumber[pMac->lim.gpLimMlmScanReq->channelList.numChannels - 1];
             }
