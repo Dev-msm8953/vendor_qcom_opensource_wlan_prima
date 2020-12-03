a5120c1460420b1fa2c2fde79b0961ad31fb699b
Albert I <kras@raphielgang.org>
2020-12-03 21:07:32 +0800

    prima: Fix build without P2P feature
    (cherry picked from commit e433ca6c534a7a46e4523a914a9ea82c22ddb64d)
    Change-Id: b7088cd377fee06e72f5aa037d9a1d62c8d18e81
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- CORE/WDI/CP/inc/wlan_qct_wdi_i.h
+++ CORE/WDI/CP/inc/wlan_qct_wdi_i.h
@@ -2473,6 +2473,7 @@ WDI_Status WDI_ProcessSetTxPowerReq
   WDI_EventInfoType*     pEventData
 );
 
+#ifdef WLAN_FEATURE_P2P
 /**
  @brief Process P2P Notice Of Absence Request function (called when Main FSM
         allows it)
@@ -2489,6 +2490,7 @@ WDI_ProcessP2PGONOAReq
   WDI_ControlBlockType*  pWDICtx,
   WDI_EventInfoType*     pEventData
 );
+#endif
 
 /**
  @brief Process TDLS Link Establish Request function (called when Main FSM
@@ -4707,6 +4709,7 @@ WDI_ProcessTXFailInd
 );
 #endif /* WLAN_FEATURE_RMC */
 
+#ifdef WLAN_FEATURE_P2P
 /**
 *@brief Process Noa Start Indication function (called when
         an indication of this kind is being received over the
@@ -4742,6 +4745,7 @@ WDI_ProcessP2pNoaAttrInd
   WDI_ControlBlockType*  pWDICtx,
   WDI_EventInfoType*     pEventData
 );
+#endif
 
 /**
 *@brief Process Tx Per Hit Indication function (called when
